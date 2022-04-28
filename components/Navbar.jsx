60c47f217493a78c31930cbc26d29f7c8dfc8c7d
Regtables <reghardt7@gmail.com>
2022-04-28 13:49:49 +0200

    implimented react context, finished cart logic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import Link from 'next/link';
 import { FiShoppingCart } from 'react-icons/fi';
 
+import { useStateContext } from '../context/StateContext';
+import { Cart } from '.';
+
 const Navbar = () => {
+	const { showCart, setShowCart, totalQuanitities } = useStateContext();
 	return (
 		<div className='navbar-container'>
 			<p className='logo'>
 				<Link href='/'>JSM Headphones</Link>
 			</p>
-			<button type='button' className='cart-icon' onClick={() => {}}>
+			<button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
       <FiShoppingCart />
-				<span className='cart-item-qty'>1</span>
+				<span className='cart-item-qty'>{totalQuanitities}</span>
 			</button>
+
+		{showCart && <Cart />}
 		</div>
 	);
 };
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,13 +5,15 @@ import {
 	AiFillStar,
 	AiOutlineStar,
 } from 'react-icons/ai';
-import { Product } from '../../components';
 
+import { Product } from '../../components';
 import { client, urlFor } from '../../lib/client';
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product;
 	const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
 	return (
 		<div>
@@ -57,13 +59,13 @@ const ProductDetails = ({ product, products }) => {
 					<div className='quantity'>
 						<h3>Quantity: </h3>
 						<p className='quantity-desc'>
-							<span className='minus' onClick=''>
+							<span className='minus' onClick= {decQty}>
 								<AiOutlineMinus />
 							</span>
-							<span className='num' onClick=''>
-								0
+							<span className='num'>
+								{qty}
 							</span>
-							<span className='plus' onClick=''>
+							<span className='plus' onClick={incQty}>
 								<AiOutlinePlus />
 							</span>
 						</p>
@@ -72,7 +74,7 @@ const ProductDetails = ({ product, products }) => {
 						<button
 							type='button'
 							className='add-to-cart'
-							onClick=''
+							onClick={() => onAdd(product, qty)}
 						>
 							Add to Cart
 						</button>
