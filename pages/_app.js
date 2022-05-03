6bff82243e5a3b0951d26dd54a4a1ac4b5e9ea22
Regtables <reghardt7@gmail.com>
2022-05-03 16:07:06 +0200

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Toaster } from 'react-hot-toast';
-
+import { client } from '../lib/client';
 import '../styles/globals.css';
 import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
